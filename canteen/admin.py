ccf91695f8c35394f6b31fd8f1ef14d791210233
nagarajpandith <nagaraj.pandith2002@gmail.com>
2022-11-25 16:08:27 +0530

    feat: extended dj user model, impl login form & logic

--- canteen/admin.py
+++ canteen/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Owner, KitchenStaff, Item, Order, OrderItem, Bill
+from .models import Account, Item, Order, OrderItem, Bill
 
 # Register your models here.
-admin.site.register(Owner)
-admin.site.register(KitchenStaff)
+admin.site.register(Account)
 admin.site.register(Item)
 admin.site.register(Order)
 admin.site.register(OrderItem)
