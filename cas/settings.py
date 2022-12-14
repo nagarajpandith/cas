a997691b985cbe08b259b690edbe3c83884d4ac4
nagarajpandith <nagaraj.pandith2002@gmail.com>
2022-12-14 11:18:07 +0530

    image-render fix

--- cas/settings.py
+++ cas/settings.py
@@ -128,8 +128,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 STATIC_URL = "static/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "static", "media")
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "static", "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
