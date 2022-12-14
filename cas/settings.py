460aa866d510dd33ab6237ed24cb3c3ec4ce1859
nagarajpandith <nagaraj.pandith2002@gmail.com>
2022-12-14 13:15:48 +0530

    minor

--- cas/settings.py
+++ cas/settings.py
@@ -29,7 +29,7 @@ load_dotenv(os.path.join(BASE_DIR, ".env"))
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
