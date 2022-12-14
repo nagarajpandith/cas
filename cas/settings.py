03de2fa72d55cc2d4fd8623a06c468fc454454e0
nagarajpandith <nagaraj.pandith2002@gmail.com>
2022-12-14 11:51:43 +0530

    whitenoise storage

--- cas/settings.py
+++ cas/settings.py
@@ -129,9 +129,14 @@ STATICFILES_DIRS = [
 ]
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "static", "media")
+
+if os.environ.get("DJANGO_ENV") == "production":
+    STATIC_URL = "https://cdn.up.railway.app/cas/static/"
+    MEDIA_URL = "https://cdn.up.railway.app/cas/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
